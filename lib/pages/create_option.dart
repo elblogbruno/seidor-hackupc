6c40c78ee9dd7f84a8bb77ef4eff27dec1d53941
Joel Marco Quiroga Poma <joelqp1@gmail.com>
2024-05-04 05:44:10 +0200

    ui creation_option done

--- lib/pages/create_option.dart
+++ lib/pages/create_option.dart
@@ -1,18 +1,84 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-class CreateOption extends StatelessWidget {
+class CreateOption extends StatefulWidget {
   const CreateOption({super.key});
+
+  @override
+  State<CreateOption> createState() => _CreateOptionState();
+}
+
+class _CreateOptionState extends State<CreateOption> {
+  String _text = "Texto inicial" * 50;
+
+  void updateText(String newText) {
+    setState(() {
+      _text = newText;
+    });
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Create Option'),
+        title: const Text('Actualización de Texto'),
       ),
-      body: const Center(
-        child: Text('Bienvenido a la Tercera Pantalla'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            const Text(
+              'Título de la Pantalla',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Flexible(
+                    child: Container(
+                      height: 300, // Altura fija del contenedor
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.blue, width: 2), // Borde del contenedor
+                        borderRadius: BorderRadius.circular(8), // Bordes redondeados
+                      ),
+                      child: SingleChildScrollView( // Permite desplazar el contenido
+                        padding: const EdgeInsets.all(16.0), // Relleno interior para el texto
+                        child: Text(
+                          _text,
+                          style: const TextStyle(fontSize: 20, color: Colors.black),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    child: UpdateTextButton(onPressed: () => updateText("Texto actualizado: ${DateTime.now()}")),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+class UpdateTextButton extends StatelessWidget {
+  final VoidCallback onPressed;
+
+  const UpdateTextButton({super.key, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: const Text('Actualizar Texto'),
+    );
+  }
+}
+
+
